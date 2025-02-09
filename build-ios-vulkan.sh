8a642aae85110fb334cd7dd69f2927fd67dadf1f
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2025-02-09 14:03:39 +0900

    add Vulkan_LIBRARY

--- build-ios-vulkan.sh
+++ build-ios-vulkan.sh
@@ -5,6 +5,7 @@ cmake .. \
     -DCMAKE_TOOLCHAIN_FILE=../toolchains/iosxc-arm64.toolchain.cmake \
     -DENABLE_BITCODE=OFF \
     -DVulkan_INCLUDE_DIR=${VULKAN_SDK}/MoltenVK/include \
+    -DVulkan_LIBRARY=${VULKAN_SDK}/MoltenVK/iOS/MoltenVK.framework/MoltenVK \
     -DNCNN_VULKAN=ON \
     -DNCNN_BUILD_BENCHMARK=OFF \
     -DCMAKE_BUILD_TYPE=Release \
