ae04a096b7844a6be0814694dfa3c71cbf894de9
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2025-06-23 22:09:14 +0900

    support vulkansdk 1.3.296

--- build-ios-vulkan.sh
+++ build-ios-vulkan.sh
@@ -4,8 +4,8 @@ cd build-ios-vulkan
 cmake .. \
     -DCMAKE_TOOLCHAIN_FILE=../toolchains/iosxc-arm64.toolchain.cmake \
     -DENABLE_BITCODE=OFF \
-    -DVulkan_INCLUDE_DIR=${VULKAN_SDK}/MoltenVK/include \
-    -DVulkan_LIBRARY=${VULKAN_SDK}/MoltenVK/iOS/MoltenVK.framework/MoltenVK \
+    -DVulkan_INCLUDE_DIR=${VULKAN_SDK}/iOS/include \
+    -DVulkan_LIBRARY=${VULKAN_SDK}/macOS/lib/MoltenVK.xcframework/ios-arm64 \
     -DNCNN_VULKAN=ON \
     -DNCNN_BUILD_BENCHMARK=OFF \
     -DCMAKE_BUILD_TYPE=Release \
